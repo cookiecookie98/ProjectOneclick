6556d02a509c79cbf108813176847980ad93315c
cookiecookie98 <tuong.cookie211@gmail.com>
2024-05-13 16:25:30 +0700

    commit 2

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -19,8 +19,8 @@ class UserModel {
         session_destroy();
     }
 
-    public static function register(){
-        $sql = "INSERT INTO `admin` (`id`, `ten`, `email`, `password_hash`, `tendangnhap`, `role`, `tokenid`, `trangthaionline`, `created_at`, `updated_at`) VALUES (NULL, 'Tuong', 'longtuong.builong18@gmail.com', 'tuong123456', 'tuongbui1', '2', '123123123123', '0', current_timestamp(), current_timestamp())";
+    public static function register($name, $email, $password, $username, $role){
+        $sql = "INSERT INTO `admin` (`id`, `ten`, `email`, `password_hash`, `tendangnhap`, `role`, `tokenid`, `trangthaionline`, `created_at`, `updated_at`) VALUES (NULL, '".$name."', '".$email."', '".$password."', '".$username."', '".$role."', '', '0', current_timestamp(), current_timestamp())";
         $result = self::$conn->prepare($sql);
         $result->execute();
     }
